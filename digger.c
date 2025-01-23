6321a97dfb73d0e08e6da7de87a3c5ef3ffeb8b8
prcoder <prcoder@gmail.com>
2025-01-23 17:51:44 +0300

    Отладка падения мешков. Рефакторинг.

--- digger.c
+++ digger.c
@@ -1155,12 +1155,10 @@ void stop_bag(struct bag_info *bag)
 {
     bag->dir = DIR_STOP; // Остановить мешок
 
-    // Проверка разбился ли мешок
-    if (bag->count > 1) // Если мешок пролетел больше одного этажа
-    {
-        bag->state = BAG_BREAKS; // То он будет разбиваться
-        bag->count = 0;
-    }
+    // Если мешок пролетел больше одного этажа, то он будет разбит
+    bag->state = (bag->count > 1) ? BAG_BREAKS : BAG_STATIONARY; // То он будет разбиваться
+
+    bag->count = 0;
 
     // TODO: Унчтожить все мешки под тем, который остановился
 }
@@ -1999,12 +1997,12 @@ void main()
                     bag_y_log = bag_abs_y_pos / POS_Y_STEP;
                     bag_y_rem = (bag_abs_y_pos % POS_Y_STEP) >> 2;
 
-                    if (bag_y_log == H_MAX - 1) stop_bag(bag); // Остановить мешок, если он долетел до последнего этажа
-                    else if (background[bag_y_log + 1][bag_x_log] == 0xFF) stop_bag(bag); // Остановить мешок, если клетка под ним не повреждена
-
                     if (bag_y_rem == 0) // Если мешок находится в центре клетки по-вертикали, значит он пролетел один этаж
                     {
                         bag->count++; // Увеличить количество этажей, которые пролетел мешок
+
+                        if (bag_y_log == H_MAX - 1) stop_bag(bag); // Остановить мешок, если он долетел до последнего этажа
+                        else if (background[bag_y_log + 1][bag_x_log] == 0xFF) stop_bag(bag); // Остановить мешок, если клетка под ним не повреждена
                     }
 
                     remove_coin(bag_x_log, bag_y_log); // Удалить монету в клетке куда попал мешок
