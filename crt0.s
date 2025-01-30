e7bccbeed78447c6e21fb9d38c664ea6c0e3bb93
prcoder <prcoder@gmail.com>
2025-01-31 00:58:07 +0300

    Переработан файл crt0.s для установки указателя стека перед стартом на ячёку перед загруженной программой.

--- crt0.s
+++ crt0.s
@@ -1,10 +1,11 @@
 	.text
-	.globl _main, ___main, _start
-
+	.globl _main, ___main, _start, ___stack_top
 _start:
-	cmp	sp, pc
-	blo	.+4
-	mov	pc, sp
+	mov pc, sp
+	sub $4, sp
+	mov $01330, r0
+	mov r0, @$0177664
+	mov	$_start,@$04
 	jsr	pc, _main
 	halt
 
