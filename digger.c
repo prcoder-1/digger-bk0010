49e7f5e406891cb6feff9aadd4d1f5023e17bb27
prcoder <prcoder@gmail.com>
2025-02-14 00:37:49 +0300

    Отладка стирания Диггера при гибели. Отладка гибели врагов при сбрасывании мешка.

--- digger.c
+++ digger.c
@@ -1781,7 +1781,7 @@ void process_bags(const uint8_t man_x_log, const uint8_t man_y_log)
                     if (bug_x_log == bag_x_log && bug->dir == DIR_UP) bug->dir = DIR_DOWN;
 
                     // Проверить, что враг попал под падающий мешок
-                    if (check_collision_4_15(bug->x_graph, bug->y_graph, bag_x_graph, bag_y_graph))
+                    if ((bag->count >= 1) && check_collision_4_15(bug->x_graph, bug->y_graph, bag_x_graph, bag_y_graph))
                     {
                         bug->state = CREATURE_DEAD_MONEY_BAG; // Враг был убит мешком с деньгами
 
@@ -2017,7 +2017,7 @@ void process_missile()
     }
 }
 
-void man_rip(int bounce);
+void man_rip();
 
 /**
  * @brief Съесть монету (драгоценный камень)
@@ -2284,7 +2284,7 @@ void process_man(const uint8_t man_x_rem, const uint8_t man_y_rem)
                     bugs_active--; // Уменьшить количество активных врагов
                     bugs_total++;  // Увеличить количество создаваемых врагов компенсируя съеденных
                 }
-                else man_rip(1);
+                else man_rip();
             }
 
             man_prev_dir = man_dir;
@@ -2308,7 +2308,7 @@ void process_man(const uint8_t man_x_rem, const uint8_t man_y_rem)
 
             if (man_dead_bag->dir == DIR_STOP)
             {
-                man_rip(0);
+                man_rip();
             }
         }
     }
@@ -2318,53 +2318,48 @@ void process_man(const uint8_t man_x_rem, const uint8_t man_y_rem)
  * @brief Подпрограмма анимации гибели Диггера, отрисовки надгробного камня и
  *        воспроизведения музыкального сопровождения
  */
-void man_rip(int bounce)
+void man_rip()
 {
-    if (bounce)
+    // Последовательность высоты на которую подпрыгивает перевёрнутый Диггер
+    static uint8_t bounce[8] = { 3, 5, 6, 6, 5, 4, 3, 0 };
+
+    uint16_t prev_y_graph = 0;
+    uint16_t period = 19000 / N;
+    uint16_t i = 0;
+    while (period < 36000 / N) // Звук убиения Диггера
     {
-        erase_4_15(man_x_graph, man_y_graph); // Стереть Диггера
+        if (snd_effects) sound(period, 2);
 
-        // Последовательность высоты на которую подпрыгивает перевёрнутый Диггер
-        static uint8_t bounce[8] = { 3, 5, 6, 6, 5, 4, 3, 0 };
-
-        uint16_t prev_y_graph = 0;
-        uint16_t period = 19000 / N;
-        uint16_t i = 0;
-        while (period < 36000 / N) // Звук убиения Диггера
+        if (man_state != CREATURE_DEAD_MONEY_BAG)
         {
-            if (snd_effects) sound(period, 2);
-
-            if (man_state != CREATURE_DEAD_MONEY_BAG)
+            uint16_t y_graph = man_y_graph - bounce[i >> 3];
+            // gnaw(DIR_UP, man_x_graph, y_graph + 1);
+            // Анимация подпрыгивающего перевёрнутого Диггера
+            // sp_paint_brick(man_x_graph, y_graph + 15, 4, 1, 0);
+            if (prev_y_graph)
             {
-                uint16_t y_graph = man_y_graph - bounce[i >> 3];
-                // gnaw(DIR_UP, man_x_graph, y_graph + 1);
-                // Анимация подпрыгивающего перевёрнутого Диггера
-                // sp_paint_brick(man_x_graph, y_graph + 15, 4, 1, 0);
-                if (prev_y_graph)
-                {
-                    sp_put(man_x_graph, prev_y_graph, sizeof(outline_digger_turned_over[0]), sizeof(outline_digger_turned_over) / sizeof(outline_digger_turned_over[0]),
-                        0, (uint8_t *)outline_digger_turned_over);
-                }
-
-                sp_put(man_x_graph, y_graph, sizeof(image_digger_turned_over[0]), sizeof(image_digger_turned_over) / sizeof(image_digger_turned_over[0]),
-                    (uint8_t *)image_digger_turned_over, (uint8_t *)outline_digger_turned_over);
-                prev_y_graph = y_graph;
-
-                // delay_ms(100);
+                sp_put(man_x_graph, prev_y_graph, sizeof(outline_digger_turned_over[0]), sizeof(outline_digger_turned_over) / sizeof(outline_digger_turned_over[0]),
+                    0, (uint8_t *)outline_digger_turned_over);
             }
 
-            if (i++ < 10)
-            {
-                period -= 1000 / N;
-            }
-            else
-            {
-                period += 500 / N;
-            }
+            sp_put(man_x_graph, y_graph, sizeof(image_digger_turned_over[0]), sizeof(image_digger_turned_over) / sizeof(image_digger_turned_over[0]),
+                (uint8_t *)image_digger_turned_over, (uint8_t *)outline_digger_turned_over);
+            prev_y_graph = y_graph;
+
+            // delay_ms(100);
         }
 
-        delay_ms(500);
+        if (i++ < 10)
+        {
+            period -= 1000 / N;
+        }
+        else
+        {
+            period += 500 / N;
+        }
     }
+
+    delay_ms(500);
 
     // TODO: Удалить врагов с которыми коллизия
     // for (uint8_t i = 0; i < bugs_max; ++i)
