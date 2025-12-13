a53c50aa29966ebf2f75a55478c94b69a845625f
prcoder <prcoder@gmail.com>
2025-12-13 22:31:42 +0300

    Работа над заставкой.

--- title.c
+++ title.c
@@ -52,6 +52,21 @@ void print_str(const char *str, uint16_t x_graph, uint16_t y_graph)
     }
 }
 
+constexpr uint16_t char_width = sizeof(ch_alpha[0][0]);
+constexpr uint16_t str_height = sizeof(ch_alpha[0]) / char_width;
+constexpr uint16_t y_space = 8;
+constexpr uint16_t windmill_height = 32;
+constexpr uint16_t table_height = SCREEN_PIX_HEIGHT - (str_height + y_space) - windmill_height;
+
+const char digger_str[] = "D I G G E R";
+constexpr uint16_t digger_str_x_pos = (SCREEN_BYTE_WIDTH - char_width * sizeof(digger_str) + char_width) / 2;
+
+const char one_str[] = "ONE";
+constexpr uint16_t one_x_pos = (SCREEN_BYTE_WIDTH + SCREEN_BYTE_WIDTH / 2 - char_width * sizeof(one_str) + char_width) / 2;
+
+const char player_str[] = "PLAYER";
+constexpr uint16_t player_x_pos = (SCREEN_BYTE_WIDTH + SCREEN_BYTE_WIDTH / 2 - char_width * sizeof(player_str) + char_width) / 2;
+
 uint16_t one_player_y;
 
 /**
@@ -59,15 +74,7 @@ uint16_t one_player_y;
  */
 void init_game()
 {
-    constexpr uint16_t char_width = sizeof(ch_alpha[0][0]);
-    constexpr uint16_t str_height = sizeof(ch_alpha[0]) / char_width;
-    constexpr uint16_t y_space = 8;
-    constexpr uint16_t windmill_height = 32;
-
     sp_paint_brick_long(0, 0, SCREEN_BYTE_WIDTH, SCREEN_PIX_HEIGHT, 0); // Очистка экрана
-
-    char digger_str[] = "D I G G E R";
-    const uint16_t digger_str_x_pos = (SCREEN_BYTE_WIDTH - char_width * sizeof(digger_str) + char_width) / 2;
 
     uint16_t y_pos = 0;
     print_str(digger_str, digger_str_x_pos, y_pos);
@@ -78,18 +85,13 @@ void init_game()
 
     one_player_y = y_pos + y_space;
 
-    const char one_str[] = "ONE";
-    const uint16_t one_x_pos = (SCREEN_BYTE_WIDTH + SCREEN_BYTE_WIDTH / 2 - char_width * sizeof(one_str) + char_width) / 2;
     print_str(one_str, one_x_pos, one_player_y);
     one_player_y += str_height + y_space;
 
-    const char player_str[] = "PLAYER";
-    const uint16_t player_x_pos = (SCREEN_BYTE_WIDTH + SCREEN_BYTE_WIDTH / 2 - char_width * sizeof(player_str) + char_width) / 2;
     print_str(player_str, player_x_pos, one_player_y);
 
     one_player_y += str_height + y_space * 2;
 
-    const uint16_t table_height = SCREEN_PIX_HEIGHT - (str_height + y_space) - windmill_height;
     sp_paint_brick_long(0, y_pos, 1, table_height, 0b00000011);
     sp_paint_brick_long(SCREEN_BYTE_WIDTH - 1, y_pos, 1, table_height, 0b11000000);
     sp_paint_brick_long(SCREEN_BYTE_WIDTH / 2, y_pos, 1, table_height, 0b11000000);
@@ -104,7 +106,8 @@ uint16_t *cur_music_ptr = nullptr;
 uint16_t demo_time = 0;
 uint16_t nobbin_x = 0, nobbin_y = 0;
 uint16_t hobbin_x = 0, hobbin_y = 0;
-bool hobbin_mirror;
+uint16_t digger_x = 0, digger_y = 0;
+bool hobbin_mirror, digger_mirror;
 uint8_t image_phase;        ///< Фаза анимации при выводе спрайта
 int8_t image_phase_inc = 1; ///< Направление изменения фазы анимации при выводе спрайта (+1 или -1)
 
@@ -117,34 +120,61 @@ void process_game_state()
     constexpr uint16_t image_height = sizeof(image_nobbin[0]) / sizeof(image_nobbin[0][0]); // Высота спрайта Ноббина
     constexpr uint16_t y_space = 8;
 
+    constexpr uint16_t start_time = 0;
+    constexpr uint16_t start_delay = 30;
+    constexpr uint16_t move_durance = 20;
+    constexpr uint16_t end_to_print = 6;
+
+    // Тайминги отображения Ноббина в демо
+    constexpr uint16_t nobbin_start_time = start_time + start_delay;
+    constexpr uint16_t nobbin_begin_time = nobbin_start_time + 1;
+    constexpr uint16_t nobbin_end_time = nobbin_begin_time + move_durance;
+    constexpr uint16_t nobbin_print_time = nobbin_end_time + end_to_print;
+
+    // Тайминги отображения Хоббина в демо
+    constexpr uint16_t hobbin_start_time = nobbin_print_time + 6;
+    constexpr uint16_t hobbin_begin_time = hobbin_start_time + 1;
+    constexpr uint16_t hobbin_end_time = hobbin_begin_time + move_durance;
+    constexpr uint16_t hobbin_mirror_time = hobbin_end_time + 1;
+    constexpr uint16_t hobbin_print_time = hobbin_mirror_time + end_to_print;
+
+    // Тайминги отображения Диггера в демо
+    constexpr uint16_t digger_start_time = hobbin_print_time + 6;
+    constexpr uint16_t digger_begin_time = digger_start_time + 1;
+    constexpr uint16_t digger_end_time = digger_begin_time + move_durance;
+    constexpr uint16_t digger_mirror_time = digger_end_time + 1;
+    constexpr uint16_t digger_print_time = digger_mirror_time + end_to_print;
+
     switch (demo_time)
     {
-        case 0:
+        case start_time:
         {
             // Очистка области Demo
+            sp_paint_brick_long(SCREEN_BYTE_WIDTH / 2 + 2, one_player_y, SCREEN_BYTE_WIDTH / 2 - 3, table_height - one_player_y, 0);
             break;
         }
 
-        case 50:
+        case nobbin_start_time:
         {
             nobbin_x = SCREEN_BYTE_WIDTH - 7;
             nobbin_y = one_player_y;
+            break;
         }
 
-        case 51 ... 71:
+        case nobbin_begin_time ... nobbin_end_time:
         {
             nobbin_x--;
             break;
         }
 
-        case 77:
+        case nobbin_print_time:
         {
             const char nobbin_str[] = "NOBBIN";
             print_str(nobbin_str, nobbin_x + image_width * 2 - 1, nobbin_y);
             break;
         }
 
-        case 84:
+        case hobbin_start_time:
         {
             hobbin_mirror = true;
             hobbin_x = SCREEN_BYTE_WIDTH - 7;
@@ -152,22 +182,49 @@ void process_game_state()
             break;
         }
 
-        case 85 ... 106:
+        case hobbin_begin_time ... hobbin_end_time:
         {
             hobbin_x--;
             break;
         }
 
-        case 112:
+        case hobbin_mirror_time:
         {
             hobbin_mirror = false;
             break;
         }
 
-        case 118:
+        case hobbin_print_time:
         {
             const char hobbin_str[] = "HOBBIN";
             print_str(hobbin_str, hobbin_x + image_width * 2 - 1, hobbin_y);
+            break;
+        }
+
+        case digger_start_time:
+        {
+            digger_mirror = true;
+            digger_x = SCREEN_BYTE_WIDTH - 7;
+            digger_y = hobbin_y + image_height + y_space;
+            break;
+        }
+
+        case digger_begin_time ... digger_end_time:
+        {
+            digger_x--;
+            break;
+        }
+
+        case digger_mirror_time:
+        {
+            digger_mirror = false;
+            break;
+        }
+
+        case digger_print_time:
+        {
+            const char digger_str[] = "DIGGER";
+            print_str(digger_str, digger_x + image_width * 2 - 1, digger_y);
             break;
         }
     }
@@ -183,6 +240,13 @@ void process_game_state()
         sp_paint_brick_long(hobbin_x + image_width, hobbin_y, 1, image_height, 0);
         if (hobbin_mirror) sp_4_15_h_mirror_put(hobbin_x, hobbin_y, (uint8_t *)image_hobbin_right[image_phase]);
         else sp_4_15_put(hobbin_x, hobbin_y, (uint8_t *)image_hobbin_right[image_phase]);
+    }
+
+    if (digger_x)
+    {
+        sp_paint_brick_long(digger_x + image_width, digger_y, 1, image_height, 0);
+        if (digger_mirror) sp_4_15_h_mirror_put(digger_x, digger_y, (uint8_t *)image_digger_right[image_phase]);
+        else sp_4_15_put(digger_x, digger_y, (uint8_t *)image_digger_right[image_phase]);
     }
 
     // Увеличить/уменьшить фазу на единицу
