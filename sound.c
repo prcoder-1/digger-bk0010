3bc591aa1714eda2f227ef72b2dda0a6bf40a326
prcoder <prcoder@gmail.com>
2026-06-08 22:52:52 +0300

    Отладка музыки на заставке. Реализация огибающей.

--- sound.c
+++ sound.c
@@ -21,6 +21,44 @@ void sound(uint16_t period, uint16_t durance)
     );
 }
 
+// Аудио через PWM при опорной частоте 1/(2*period). За один полный аудио-цикл
+// (2*period sob-тактов) динамик включён на `pw` тактов, выключен на остальные
+// (2*period - pw). Высота тона определяется period, а перцептивная громкость
+// - дробью pw/(2*period). Используется для построения огибающей: вызывающий
+// C-код делит ноту на несколько стадий и для каждой передаёт свой pw.
+//
+// Допустимый диапазон: 1 <= pw <= 2*period - 1. При нарушении внутренние sob
+// получают 0 и крутят 65536 итераций - C-обёртка обязана клампить.
+//
+// durance считается так же, как в sound_vibrato (число полупериодов): здесь
+// делится на 2, чтобы получить число полных аудио-циклов.
+void sound_pwm(uint16_t period, uint16_t durance, uint16_t pw)
+{
+    asm volatile (
+        "mov %[period], r1\n\t"
+        "mov %[durance], r2\n\t"
+        "mov %[pw], r5\n\t"
+        "asr r2\n\t"                  // полупериоды -> полные циклы
+        "beq .l_end_%=\n"             // меньше одного цикла - выход
+".l1_%=:\n\t"
+        "mov $0100, @$-062\n\t"       // динамик ON
+        "mov r5, r4\n"
+".l_on_%=:\n\t"
+        "sob r4, .l_on_%=\n\t"
+        "clr @$-062\n\t"              // динамик OFF
+        "mov r1, r4\n\t"
+        "asl r4\n\t"                  // r4 = 2*period
+        "sub r5, r4\n"                // r4 = 2*period - pw
+".l_off_%=:\n\t"
+        "sob r4, .l_off_%=\n\t"
+        "sob r2, .l1_%=\n"
+".l_end_%=:\n\t"
+        :
+        : [period]"g"(period), [durance]"g"(durance), [pw]"g"(pw)
+        : "r1", "r2", "r4", "r5", "cc", "memory"
+    );
+}
+
 void sound_vibrato(uint16_t period, uint16_t durance)
 {
     asm volatile (
--- sound.h
+++ sound.h
@@ -16,3 +16,19 @@ void sound(uint16_t period, uint16_t durance);
  * @param durance - длительность звука
  */
 void sound_vibrato(uint16_t period, uint16_t durance);
+
+/**
+ * @brief Вывод тона с заданной шириной импульса (PWM, для огибающей).
+ *
+ * Один аудио-цикл = 2*period sob-тактов: динамик ON на pw, OFF на
+ * (2*period - pw). Громкость пропорциональна pw/(2*period); pw=period
+ * даёт максимум (50% duty). Чтобы внутренние sob-петли не зациклились,
+ * вызывающий код обязан гарантировать 1 <= pw <= 2*period - 1.
+ *
+ * durance - число полупериодов (как у sound_vibrato).
+ *
+ * @param period - период звука (1 / частота)
+ * @param durance - длительность звука
+ * @param pw     - ширина «ON»-фазы в sob-тактах
+ */
+void sound_pwm(uint16_t period, uint16_t durance, uint16_t pw);
